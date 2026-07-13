96f5799013a2b17be721556835124b7619704354
lch <lch@lchs-MacBook-Air.local>
2026-07-14 02:00:39 +0800

    fix(ios): 链接 AudioToolbox/AVFoundation 框架修复 iOS 构建

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,10 @@
 fn main() {
-    tauri_build::build()
+    tauri_build::build();
+
+    #[cfg(target_os = "ios")]
+    {
+        println!("cargo:rustc-link-lib=framework=AudioToolbox");
+        println!("cargo:rustc-link-lib=framework=AVFoundation");
+        println!("cargo:rustc-link-lib=framework=Foundation");
+    }
 }
